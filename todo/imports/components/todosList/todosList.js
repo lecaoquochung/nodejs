0b259ef5c2a8fddc40e26d5624bffc238952777b
hungle <lecaoquochung@gmail.com>
2016-12-13 16:01:24 +0900

    [todo] form show newest task first

--- todo/imports/components/todosList/todosList.js
+++ todo/imports/components/todosList/todosList.js
@@ -4,21 +4,45 @@ import template from './todosList.html';
 import { Tasks } from '../../api/tasks.js';
 
 class TodosListCtrl {
-  constructor($scope) {
-    $scope.viewModel(this); // ?
 
-    this.helpers({
-      tasks() {
-        return Tasks.find({}); // find in db
-      }
-    })
-  }
+    // constructor
+    // TODO How to use that thing?
+    // var $scope, this.helpers
+    constructor($scope) {
+        $scope.viewModel(this);
+
+        this.helpers({
+            tasks() {
+                // return Tasks.find({}); // find in db
+
+                // Show newest tasks at the top
+                return Tasks.find({}, {
+                    sort: {
+                        createdAt: -1
+                    }
+                });
+            }
+        })
+    }
+
+    // add task
+    addTask(newTask) {
+        // Insert a task into the collection
+        Tasks.insert({
+            text: newTask,
+            createdAt: new Date
+        });
+
+        // Clear form
+        this.newTask = '';
+    }
+
 }
 
 export default angular.module('todosList', [
-  angularMeteor
-])
-  .component('todosList', {
-    templateUrl: 'imports/components/todosList/todosList.html',
-    controller: TodosListCtrl
-  });
+        angularMeteor
+    ])
+    .component('todosList', {
+        templateUrl: 'imports/components/todosList/todosList.html',
+        controller: TodosListCtrl
+    });
